3043e9b376e7d021a8560798129bbd00c57eb338
Ioana Culic <ioana_mculic@yahoo.com>
2015-01-13 22:49:44 +0200

    database functions

--- database.js
+++ database.js
@@ -2,60 +2,176 @@
 
 var config = require('./config.js').data;
 var mysql = require('mysql');
+var uuid = require('node-uuid');
 
 var connection = mysql.createConnection({
   host     : config.host,
   user     : config.username,
   password : config.password,
-  database	: config.database
+  database : config.database
 });
 
-var dashboardName = config.prefix+"dashboard";
-var graphName = config.prefix+"graph";
+var dashboardTable = config.prefix+"dashboard";
+var graphTable = config.prefix+"graph";
 
 connection.connect();
 
-
-function addDashboard(name, description)
+function parseName(name)
 {
-	var query = "insert into "+dashboardName+" (name, description) values ("+mysql.escape(name)+
-				", "+mysql.escape(description)+");";
+	name = name.toLowerCase();
+	for(var i=0; i<name.length; i++)
+	{
+		var c = name.charAt(i);
+		if(!((c>='a' && c<='z') || c=='_' || (c>='0' && c<='9')))
+		{
+			name = name.substring(0,i)+'_'+name.substring(i+1);
+		}  
+	}
+	return name;
+}
+
+function addDashboard(name, description,callbackFunction)
+{
+	name = parseName(name);
+	var uid = uuid.v4();
+	for(var i=0; i<uid.length; i++)
+	{
+		var c = uid.charAt(i);
+		if(!((c>='a' && c<='z') || (c>='0' && c<='9')))
+		{
+			uid =  uid.slice(0, i) + uid.slice(i+1);
+		}
+	}
+	var query = "insert into "+dashboardTable+" (name, description, uuid) values ("+mysql.escape(name)+
+				", "+mysql.escape(description)+", "+mysql.escape(uid)+");";
+console.log(query);
 	connection.query(query, function(err, rows){
 		if(err)
 			console.log("Could not add entry to database "+err);
+		callbackFunction(err);
 	});
 }
 
-function addGraph(name, description, unit, dashboard, fnct)
+addDashboard("my_dash",null, function(err){
+	addGraph("my_graph","alabalaportocala","celsius","3f0ff286b5ba4238976c715c4cf598c2",function(err){});
+});
+
+function addGraph(name, description, unit, dashboard, callbackFunction)
 {
-	var query = "insert into "+graphName+" (name, description, unit, dashboard) values ("+mysql.escape(name)+
+	name = parseName(name);
+	var signalTable = config.prefix+dashboard+name;
+	var query = "insert into "+graphTable+" (name, description, unit, dashboard) values ("+mysql.escape(name)+
 				", "+mysql.escape(description)+", "+mysql.escape(unit)+", "+mysql.escape(dashboard)+");";
 	connection.query(query, function(err, rows){
 		if(err)
 			console.log("Could not add entry to database "+err);
-		else
-		{
-			var dataName = config.prefix+dashboard+name;
-			query = "create table "+dataName+" (timestamp TIMESTAMP NOT NULL, value DOUBLE NOT NULL, PRIMARY KEY(timestamp));";
-			connection.query(query, function(err, rows){
-				if(err)
-					console.log("Could not add entry to database "+err);
-			});
-		}
-		fnct(err);
+		// else
+		// {
+		// 	// query = "create table "+signalTable+" (timestamp TIMESTAMP(3) NOT NULL, value DOUBLE NOT NULL, PRIMARY KEY(timestamp));";
+		// 	// connection.query(query, function(err, rows){
+		// 	// 	if(err)
+		// 	// 		console.log("Could not add entry to database "+err);
+		// 	// });
+		// }
+		callbackFunction(err);
 	});
 }
 
-function addSignal(timestamp, value, graph, dashboard)
+function addSignal(timestamp, value, dashboard, callbackFunction)
 {
 	var dataName = config.prefix+dashboard+graph;
-	var query = "insert into "+dataName+" (timestamp, value) values ("+timestamp+", "+
+	var query = "insert into "+dataName+" (timestamp, value) values (from_unixtime("+timestamp+"), "+
 				mysql.escape(value)+");";
 	connection.query(query, function(err, rows){
 		if(err)
 			console.log("Could not add entry to database "+err);
+		callbackFunction(err);
 	});
 }
-addGraph("g",null,"celsius",2, function(err){
-	addSignal(1234,12,'g', 2);
-});
+
+function deleteDashboard(dashboardId, callbackFunction)
+{
+	var dashboardTable = config.prefix + 'dashboard';
+	var signalsTable = config.prefix+mysql.escape(dashboardId);
+	var query = "delete from "+dashboardTable+" where id = "+mysql.escape(dashboardId)+";";
+	var tables = "";
+	connection.query(query, function(err, rows){
+		if(err)
+			console.log("Could not delete dasboard");
+		else
+		{
+			query = "show tables;";
+			connection.query(query, function(err, rows){
+				if(!err)
+				{
+					for(var i=0; i<rows.length; i++)
+					{
+						var row = rows[i]['Tables_in_'+config.database];
+						if(row.substring(0,signalsTable.length) == signalsTable)
+						{
+							tables = tables+row+", ";
+						}
+					}
+					tables = tables.substring(0,tables.length-2);
+					query = "drop table if exists "+tables+";";
+					connection.query(query, function(err){
+						callbackFunction(err);
+					});
+				}
+			})
+		}
+	});
+}
+
+function deleteGraphFromDashboard(graphId, dashboardId, callbackFunction)
+{
+	var graphTable = config.prefix+'graph';
+	getGraphName(graphId, function(err, name){
+		if(!err && name)
+		{
+			var signalsTable = config.prefix+mysql.escape(dashboardId)+name;
+			var query = "delete from "+graphTable+" where id = "+graphId+";";
+			connection.query(query, function(err, rows){
+				if(!err)
+				{
+					query = "drop table if exists "+signalsTable+";";
+					connection.query(query,function(err, rows){
+						callbackFunction(err);
+					});
+				}
+				else
+				{
+					console.log("Could not delete graph");
+					callbackFunction(err);
+				}
+			});
+		}
+		else
+		{
+			console.log("Graph does not exist");
+			callbackFunction(-1);
+		}
+	});
+}
+
+function getGraphName(graphId, callbackFunction)
+{
+	var graphTable = config.prefix+'graph';
+	var query = "select name from "+graphTable+" where id = "+mysql.escape(graphId)+";";
+	connection.query(query, function(err,rows){
+		if(!err && rows.length>0)
+		{
+			callbackFunction(null,rows[0].name);
+		}
+		else
+			callbackFunction(err, null);
+	})
+}
+
+
+
+// addSignal(1196440219,12,'g', 2, function(err){
+// 	console.log(err);
+// });
+
+//deleteDashboard(1, function(err){});
--- database_init.js
+++ database_init.js
@@ -28,18 +28,19 @@ function createGraphs()
 				if(rows.length==0)
 				{
 					connection.query("CREATE  TABLE "+graphsName+"(id INT NOT NULL AUTO_INCREMENT,"+
-						"name VARCHAR(45) NOT NULL,description VARCHAR(45) NULL,unit VARCHAR(45) NOT NULL,dashboard INT NOT NULL,"+
+						"name VARCHAR(45) NOT NULL,description VARCHAR(45) NULL,unit VARCHAR(45) NOT NULL,dashboard VARCHAR(150) NOT NULL,"+
 					"PRIMARY KEY (id),INDEX dashboard (dashboard ASC),CONSTRAINT dash"+
 					" FOREIGN KEY (dashboard) REFERENCES "+dashboardName+"(id)"+
 					" ON DELETE CASCADE\n ON UPDATE CASCADE);", function(err, rows){
-						console.log(err);
+						//console.log(err);
 						if(err)
-							console.log('cannot create table');
-						connection.end();
+							console.log('cannot create table '+err);
+						else
+							createSignals();
 					});
 				}
 				else
-					connection.end();
+					createSignals();
 			}
 			else
 			{
@@ -49,18 +50,72 @@ function createGraphs()
 		});
 }
 
+function createSignals()
+{
+	var signalTable = PREFIX+'signal';
+	connection.query("show tables like '"+signalTable+"'", function(err, rows){
+		if(!err)
+		{
+			if(rows.length == 0)
+			{
+				var query = "CREATE  TABLE "+signalTable+" (id INT NOT NULL AUTO_INCREMENT, name VARCHAR(45) NULL, "+
+				"dashboarduuid INT NOT NULL ,PRIMARY KEY (id), CONSTRAINT uc_name_dash UNIQUE (name, dashboarduuid) );";
+				connection.query(query,function(err,rows){
+					if(err)
+						console.log('cannot create table '+err);
+					else
+						createCorrespondence();
+				});
+			}
+			else
+			{
+				createCorrespondence();
+			}
+		}
+		console.log(err);
+	});
+}
+
+function createCorrespondence()
+{
+	console.log("corresp");
+	var correspondenceTable = PREFIX+'correspondence';
+	var signalTable = PREFIX+'signal';
+	var graphTable = PREFIX+'graph';
+	connection.query("show tables like '"+correspondenceTable+"'", function(err, rows){
+		if(!err)
+		{
+			if(rows.length == 0)
+			{
+				console.log("create");
+				var query = "CREATE  TABLE "+correspondenceTable+"(signalid INT NOT NULL ,graphid INT NOT NULL, "+
+				"INDEX fk1 (signalid, graphid), CONSTRAINT f1 FOREIGN KEY (signalid) REFERENCES "+
+				signalTable+" (id) ON DELETE CASCADE ON UPDATE CASCADE, CONSTRAINT f2 FOREIGN KEY (graphid) REFERENCES "+
+				graphTable+"(id) ON DELETE CASCADE ON UPDATE CASCADE);";
+	console.log(query);
+				connection.query(query,function(err,rows){
+					if(err)
+						console.log("Cannot create database "+err);
+				});
+			}
+		}
+		console.log(err);
+		connection.end();
+	}); 
+}
+
 connection.query("show tables like '"+dashboardName+"'", function(err, rows)
 {
 	if(!err)
 	{
 		if(rows.length == 0)
 		{
-			var tableName = PREFIX+dash
+			var tableName = PREFIX+'dashboard';
 			connection.query("CREATE  TABLE "+dashboardName+"(id INT NOT NULL AUTO_INCREMENT ,"+
-							"name VARCHAR(45) NOT NULL ,description VARCHAR(45) NULL , PRIMARY KEY (id));",
+							"name VARCHAR(45) NOT NULL ,description VARCHAR(45) NULL, uuid VARCHAR(150),PRIMARY KEY (id), UNIQUE INDEX uuid (uuid));",
 			function(err, rows){
 				if(err)
-					console.log('cannot create table');
+					console.log('cannot create table '+err);
 				else
 				{
 					createGraphs();
@@ -72,7 +127,7 @@ connection.query("show tables like '"+dashboardName+"'", function(err, rows)
 	}
 	else
 	{
-		console.log(err)
+		console.log(err);
 		connection.end();
 	}
 });
